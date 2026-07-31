df79e113a04596f3c5a88a61bc84a8632ca3305e
Aluizio Tomazelli <aluiziotomazelli@gmail.com>
2026-07-31 18:40:27 -0400

    test: update MockNvsCore with process_boot_reasons and create_default_storage

--- host_test/common/mock_nvs_core.hpp
+++ host_test/common/mock_nvs_core.hpp
@@ -11,4 +11,10 @@ class MockNvsCore : public INvsCore
 public:
     MOCK_METHOD(esp_err_t, load_core, (CoreStorage & core), (override));
     MOCK_METHOD(esp_err_t, save_core, (const CoreStorage& core, bool force_nvs_commit), (override));
+    MOCK_METHOD(
+        void,
+        process_boot_reasons,
+        (CoreStorage & core, esp_reset_reason_t reset_reason, esp_sleep_wakeup_cause_t wakeup_cause, bool& out_pending_commit),
+        (override));
+    MOCK_METHOD(esp_err_t, create_default_storage, (CoreStorage & core, const CoreStorage& default_core), (override));
 };
